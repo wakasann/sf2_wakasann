bfffd28c1f6b90056f4b71553bd1deaca3a09dcf
wakasann <xiaofosong@126.com>
2016-03-29 21:47:04 +0800

    maybe nothing

--- src/Hello/WebBundle/Controller/DefaultController.php
+++ src/Hello/WebBundle/Controller/DefaultController.php
@@ -12,6 +12,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Hello\WebBundle\Entity\Book;
 use Hello\WebBundle\Entity\User;
 use Hello\WebBundle\Entity\Profile;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/")
@@ -50,4 +51,40 @@ class DefaultController extends Controller
 
         return array('name' => 'a','form'=>$form->createView());
     }
+
+    /**
+     * @Route("/myresult")
+     * @Template()
+     */
+    public function myrequestAction(){
+
+        $request = Request::createFromGlobals();
+
+// the URI being requested (e.g. /about) minus any query parameters
+        echo $request->getPathInfo();
+
+
+// retrieve GET and POST variables respectively
+        $request->query->get('foo');
+        $request->request->get('bar', 'default value if bar does not exist');
+
+// retrieve SERVER variables
+        $request->server->get('HTTP_HOST');
+
+// retrieves an instance of UploadedFile identified by foo
+        $request->files->get('foo');
+
+// retrieve a COOKIE value
+        $request->cookies->get('PHPSESSID');
+
+// retrieve an HTTP request header, with normalized, lowercase keys
+        $request->headers->get('host');
+        $request->headers->get('content_type');
+
+        $request->getMethod();    // GET, POST, PUT, DELETE, HEAD
+        $request->getLanguages(); // an array of languages the client accepts
+
+        return array();
+    }
+
 }
